c5782a15d1fbc013e9e9c339de12c8f03b4905e3
mk@Shiva <gangwar3537@gmail.com>
2022-08-01 17:56:56 +0530

    port updated

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(flash());
 app.use(customSetFlashMWare.setFlash);
 app.use('/', require('./routes'));
 app.use(express.static('assets'));
-app.listen(process.env.port || port,(err)=>{
+app.listen(process.env.PORT || port,(err)=>{
     if(err){
         console.log(`Error in running the server: ${err}`);
         return;
